19c1e56a59f4c58872ed3da2b8471661ee37c5b4
Tanuja14 <talekartanuja14@gmail.com>
2026-02-05 09:58:08 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,10 @@
+class add
+{
+    public static void main(String[] args) {
+        int a = 14;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+
+    }
+}
---
+++ swapwithTemp.java
@@ -0,0 +1,18 @@
+class swapwithTemp {
+    public static void main(String orgs[]) {
+        int a = 14;
+        int b = 10;
+        int temp;
+        System.out.println("Swaped numbers:");
+        temp = a;
+        a = b;
+        b = temp;
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+       
+    }
+}
+
+
+
